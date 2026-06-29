cd28175eaa5dd1f8ddb9686b274a5f5534053f89
IgaP-faey <iga.poplawska0@gmail.com>
2026-06-29 23:08:08 +0200

    the player movement
    the player movement has been added. the player is doing little jumps, so i guess he is a jumper now. no idea how to fix it, so i am leaving it as it is and making it as part of him

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 10.0f;
+    public float jumpForce = 10.0f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float movex = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2 (movex*moveSpeed,-3.0f);
     }
 }
